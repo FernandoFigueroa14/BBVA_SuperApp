8e7eb7acdb9caf91d1a67c0247be7d4b04315ebb
Carlos Pano H <carlospano8@gmail.com>
2021-10-22 23:05:09 -0500

    Navigation

--- screens/view_login.js
+++ screens/view_login.js
@@ -1,8 +1,15 @@
-import React from "react";
-import {StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-const HomeScreen = () => {
+const view_login = () => {
+    return (
+        <View>
+            <Text>Hola Mundo</Text>
+            <Text></Text>
+        </View>
+    )
+}
 
-};
+export default view_login
 
-export default HomeScreen;
+const styles = StyleSheet.create({})
