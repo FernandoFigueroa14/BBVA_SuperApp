8e5bd69c6d926f5810221c8d8b55b3c61ee738e9
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-10-23 01:55:08 -0500

    ok

--- screens/SaludFinanciera.js
+++ screens/SaludFinanciera.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-
-//import Pie from 'react-native-pie';
-
-//console.disableYellowBox = true;
-
-const SaludFinanciera = () => {
-  return (
-   <Text>Hola</Text>
-    
-  )
-=======
-
-  
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
@@ -24,7 +8,6 @@ const SaludFinanciera = () => {
             
         </View>
     )
->>>>>>> 217a1936a66b3cd8571b95fd2d128421a727ac03
 }
 
 export default SaludFinanciera;
