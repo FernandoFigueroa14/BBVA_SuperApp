f8e3143da3398c58e31632ca483897ac731030d8
Carlos Pano H <carlospano8@gmail.com>
2021-10-23 00:18:54 -0500

    Tabs Menu

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -3,8 +3,18 @@ import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity} from 
 
 
 //Carga de screens
+import Cuenta from '../screens/Cuenta';
+import Fastpay from '../screens/FastPay';
+import Home from '../screens/Home';
+import Inversiones from '../screens/Inversiones';
 import Loader from '../screens/Loader';
-import view_login from '../screens/view_login';
+import Login from '../screens/Login';
+import Movimientos from '../screens/Movimientos';
+import PagoServicios from '../screens/PagoServicios';
+import Perfil from '../screens/Perfil';
+import SaludFinanciera from '../screens/SaludFinanciera';
+import ScanQR from '../screens/ScanQR';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -22,19 +32,38 @@ const Tabs = () => {
                 backgroundColor: '#ffffff',
                 borderRadius: 15,
                 height:90,
+                ... styles.shadow
             }
         }}
         >
-            <Tab.Screen name="Home" component={Loader} />
-            <Tab.Screen name="Home" component={view_login} />
+            <Tab.Screen name="Home" component={Home} options={{
+                tabBarIcon: ({focused}) =>(
+                    <View>
+                        <Image source={require('../Front_Design/Icons/home(1).png')}
+                        />
+                        <Text>HOME</Text>
+                    </View>
+                ),
+            }} />
+            <Tab.Screen name="SaludFinanciera" component={SaludFinanciera} />
+            <Tab.Screen name="Fastpay" component={Fastpay} />
+            <Tab.Screen name="Inversiones" component={Inversiones} />
+            <Tab.Screen name="PagoServicios" component={PagoServicios} />
         </Tab.Navigator>
     );
 }
 
 const styles = StyleSheet.create({
     shadow: {
-        shadowColor: '#7F5Df0'
+        shadowColor: '#7F5Df0',
+        shadowOffset:{
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5
     }
-})
+});
 
 export default Tabs;
