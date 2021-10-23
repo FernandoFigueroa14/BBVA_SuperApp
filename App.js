49557ecf4c1856984c0406f31263bb77401918a1
Ernesto Guevara <30807748+ErnestoGuevara@users.noreply.github.com>
2021-10-22 23:34:26 -0500

    navigation

--- App.js
+++ App.js
@@ -3,9 +3,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground,Image } from 'react-native';
 import Loader from './screens/Loader';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import view_login from './screens/view_login';
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return( <Loader/>)
+
+
+  return(<NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="login" component={view_login}></Stack.Screen>
+    <Stack.Screen name="Loader" component={Loader}></Stack.Screen>
+    
+
+    </Stack.Navigator>
+   
+  </NavigationContainer> 
+  )
   
 }
 
