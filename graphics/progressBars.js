a9ad11d946fc4c7a7870d42400b1bef8072d0fba
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-10-23 20:21:35 -0500

    SaludFinanciera

--- graphics/progressBars.js
+++ graphics/progressBars.js
@@ -1,16 +1,37 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import { ProgressBar, Colors } from 'react-native-paper';
+import * as React from 'react'
+import { View } from 'react-native'
+import { ProgressBar, Colors } from 'react-native-paper'
 
-var income = 1.0;
-var expenses = 0.6;
+var income = 1.0
+var expenses = 0.6
 
 const ProgressBars = () => (
-    <View>
-        <ProgressBar progress={income} color={Colors.red800} />
-        <ProgressBar progress={income} color={Colors.red800} />
-    </View>
-  
-);
+  <View style={{
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 20,
+        flexWrap: 'nowrap',
+  }}>
+    <ProgressBar
+      style={{ 
+        marginHorizontal: 15,
+        height: 15,
+        width: 300,
+        borderRadius:15
+        }}
+      progress={income}
+      color={Colors.red800}
+    />
+    <ProgressBar 
+    style={{ 
+        marginHorizontal: 15,
+        height: 15,
+        width: 300,
+        borderRadius:15
+        }}
+    progress={expenses} color="#FFAE00" />
+  </View>
+)
 
-export default ProgressBars;
+export default ProgressBars
